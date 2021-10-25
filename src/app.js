0b3fc6561c095e31e96668ea01a0228980dcb100
RandomOtuzBir <salihdegirmenci1999@gmail.com>
2021-10-25 15:41:11 +0300

    Design Changed

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
+let createButton31 = document.getElementById("createRandom31");
+let createButton331 = document.getElementById("createRandom331");
+let createRandomWithLength = document.getElementById("createRandomWithLength");
+let randomLengthInput = document.getElementById("randomLength");
+let randomLength = 0;
+
 function makeRandom31() {
   let random31 = "";
   let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
@@ -6,7 +12,8 @@ function makeRandom31() {
     random31 += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   let randomDiv = document.getElementById("randomDiv");
-  randomDiv.innerHTML = random31;
+  //randomDiv.innerHTML = random31;
+  randomDiv.value = random31;
   console.log("anan");
   //document.write(random31);
 }
@@ -18,13 +25,31 @@ function makeRandom331() {
     random31 += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   let randomDiv = document.getElementById("randomDiv");
-  randomDiv.innerHTML = random31;
+  //randomDiv.innerHTML = random31;
+  randomDiv.value = random31;
+  console.log("anan");
+  //document.write(random31);
+}
+function makeRandomWithLength() {
+  let random31 = "";
+  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  let charactersLength = characters.length;
+  for (let i = 0; i < randomLength; i++) {
+    random31 += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  let randomDiv = document.getElementById("randomDiv");
+  //randomDiv.innerHTML = random31;
+  randomDiv.value = random31;
   console.log("anan");
   //document.write(random31);
 }
 
-let createButton31 = document.getElementById("createRandom31");
+function setLength() {
+  let randomLengthNew = document.getElementById("randomLength");
+  randomLength = randomLengthNew.value;
+}
+
+randomLengthInput.addEventListener("change",setLength);
 createButton31.addEventListener("click", makeRandom31);
-let createButton331 = document.getElementById("createRandom331");
 createButton331.addEventListener("click", makeRandom331);
-//makeRandom31();
+createRandomWithLength.addEventListener("click", makeRandomWithLength);
